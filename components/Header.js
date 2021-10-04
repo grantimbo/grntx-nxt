81b6bb888bf2316515848fea74f64ed6daabd9ba
grantimbo <grant.imbo@gmail.com>
2021-10-05 01:03:24 +0800

    add Tawkto

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,22 @@
-import Link from "next/link";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-export default function Headers() {
+const Header = () => {
   const router = useRouter();
+  useEffect(() => {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date();
+    var s1 = document.createElement("script"),
+      s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = "https://embed.tawk.to/6136161ad6e7610a49b3e466/1fetkcm4v";
+    s1.charset = "UTF-8";
+    s1.setAttribute("crossorigin", "*");
+    s0.parentNode.insertBefore(s1, s0);
+  }, []);
 
-  return (
+  https: return (
     <header>
       <Link href="/">
         <a>
@@ -39,4 +51,6 @@ export default function Headers() {
       </nav>
     </header>
   );
-}
+};
+
+export default Header;
